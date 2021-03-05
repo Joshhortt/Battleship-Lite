d46d98c4fac9667f4b5b9f526a68adf2d03ac1cb
Joshhortt <joshhortt@yahoo.com>
2021-03-05 11:43:48 +0000

    Add new project - Console Application - BattleshipLite + comments.

---
+++ BattleshipLiteApp/BattleshipLite/Program.cs
@@ -0,0 +1,17 @@
+﻿//II - Add new project Console Aplication
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLite
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ReadLine(); //  1. add this
+		}
+	}
+}
