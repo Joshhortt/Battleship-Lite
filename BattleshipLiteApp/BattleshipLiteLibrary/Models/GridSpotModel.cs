320da7dfab03784d224342ca959ffb6006d8c408
Joshhortt <joshhortt@yahoo.com>
2021-03-05 10:46:04 +0000

    Fc. Add Models directory with two classes containing properties.

---
+++ BattleshipLiteApp/BattleshipLiteLibrary/Models/GridSpotModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary.Models
+{
+	public class GridSpotModel  // 2. add public
+	{
+		// 3. add properties 
+		public string SpotLetter { get; set; }
+		public int SpotNumber { get; set; }
+		public int Status { get; set; }  // 0 = empty, 1 = ship, 2 = miss, 3 = hit, 4 =
+	}
+}
---
+++ BattleshipLiteApp/BattleshipLiteLibrary/Models/PalayerInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary.Models
+{
+	public class PalayerInfoModel // 1. add public
+	{
+		// 4. properties
+		public string UserName { get; set; }
+		public List<GridSpotModel> ShipLocation { get; set; }
+		public List<GridSpotModel> ShotGrid { get; set; }
+	}
+}
