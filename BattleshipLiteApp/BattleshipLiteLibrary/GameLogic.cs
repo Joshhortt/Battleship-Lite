46ea362e011942be1dc834d81e7521de866a8557
Joshhortt <joshhortt@yahoo.com>
2021-03-05 16:37:40 +0000

    Add new class 'GameLogic' to 'BattleshipLiteLibrary' + new method & 2 Lists for letters & numbers.

---
+++ BattleshipLiteApp/BattleshipLiteLibrary/GameLogic.cs
@@ -0,0 +1,45 @@
+﻿//II - (cont.) after adding new project Console Aplication
+using BattleshipLiteLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary
+{
+	public class GameLogic  // 7. add public
+	{
+		public static void InitializeGrid(PlayerInfoModel model)  // 8. add new method
+		{
+			List<string> letters = new List<string>  // 9. add ne List of letters
+			{
+				"A",
+				"B",
+				"C",
+				"D",
+				"E"
+			};
+
+			List<int> numbers = new List<int>  // 10. add ne List of numbers
+			{
+				1,
+				2,
+				3,
+				4,
+				5
+			};
+			// 11. Adding Grid Items
+			foreach(string item in letters)  // 12. loop through to every single letter
+			{
+				foreach (int number in numbers)  // 13. Inside each letter we re going to loop through to every single number
+				{ 
+				}
+			}
+		}
+		private static void AddGridSpot(PlayerInfoModel model, string letter, string number) // 14. add new method
+		{
+			                                                                                     
+		}
+	}
+}
